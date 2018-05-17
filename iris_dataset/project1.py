453b9ed047c26340ed8646da222e0713c13a5b77
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-05-17 19:50:40 +0530

    Update project1.py

--- iris_dataset/project1.py
+++ iris_dataset/project1.py
@@ -5,10 +5,8 @@ from numpy import exp, array, random, dot
 input_data = []
 output_data = []
 
-#PREPARE INPUT AND OUTPUT DATASET
 def prepare_input_and_output_data():
-	#READ THE CSV FILE
-	filename = 'iris.csv'
+	filename = 'iris2.csv'
 	raw_data = open(filename, 'r')
 	reader = csv.reader(raw_data)
 	#READING DATA AND CREATING THE DATASET
@@ -19,8 +17,6 @@ def prepare_input_and_output_data():
 		temp_row.append(row)
 		column.append(float(temp_row[x][0]))
 		column.append(float(temp_row[x][1]))
-		column.append(float(temp_row[x][2]))
-		column.append(float(temp_row[x][3]))
 		input_data.append(column)
 		var=temp_row[x][4]
 		if var=="setosa":
@@ -30,57 +26,49 @@ def prepare_input_and_output_data():
 		elif var=="virginica":
 			output_data.append(2)
 		x=x+1
-	#print(output_data)
 
 class NeuralNetwork(object):
 	def __init__(self):
 		random.seed(1)
-		#GENERATING RANDOM WEIGHTS
 		self.weights0 = 2 * random.random((2,3)) - 1
-		self.weights1 = 2 * random.random((3,150)) - 1
-		# print(self.weights0)
-		# print(self.weights1)
-	#SIGMOID ACTIVATION FUNCTION
+		self.weights1 = 2 * random.random((3,1)) - 1
+
 	def sigmoid(self,x):
 		return 1/(1+exp(-x))
-	#DERIVATIVE OF SIGMOID FUNCTION
+
 	def sigmoid_derivative(self,x):
 		return x*(1-x)
-	#NETWORK THINKS
-	def think(self, inputs, weights=None):
-		if weights is not None:
-			return self.sigmoid(dot(inputs, weights))
-		else:
-			return self.sigmoid(dot(inputs, self.weights0))
-	#TRAINING NETWORK
-	def train(self, layer0, training_set_outputs, number_of_training_iterations):
-		for iteration in range(number_of_training_iterations):
-			layer1 = self.think(layer0, self.weights0)
-			layer2 = self.think(layer1, self.weights1)
-			#NOW CALCULATING ERRORS
-			layer2_error=training_set_outputs-layer2
-			# #BACKPROPOGATION OF ERRORS
-			layer2_delta=layer2_error*self.sigmoid_derivative(layer2)
-			layer1_error=layer2_delta.dot(self.weights1.T)
-			layer1_delta=layer1_error*self.sigmoid_derivative(layer1)
-			#GRADIENT DESCENT USING DELTAS
-			self.weights1 += layer1.T.dot(layer2_delta)*.1
-			self.weights0 += layer0.T.dot(layer1_delta)*.1
-		#self.dimensions(layer0,self.weights0,layer1,self.weights1,layer2)
-	#PRINT DIMENSIONS OF MATRICES FORMED
-	def dimensions(self,l0,w0,l1,w1,l2):
+
+	def think(self, inputs, weights):
+		return self.sigmoid(dot(inputs, weights))
+
+	def print_dimensions(self,l0,w0,l1,w1,l2):
 		print(len(l0),len(l0[0]))
 		print(len(w0),len(w0[0]))
 		print(len(l1),len(l1[0]))
 		print(len(w1),len(w1[0]))
 		print(len(l2),len(l2[0]))
 
+	def train(self, layer0, training_set_outputs, iterations):
+		for iteration in range(iterations):
+			layer1 = self.think(layer0, self.weights0)
+			layer2 = self.think(layer1, self.weights1)
+			#BACKPROPOGATION
+			layer2_error=training_set_outputs-layer2
+			#hidden_error = layer1 * (1 - layer1) * dot(layer2_error, self.weights1.T)
+		self.print_dimensions(layer0,self.weights0,layer1,self.weights1,layer2)
+
+	def prediction(self):
+		pred=training_set_inputs[140]
+		layer1 = self.think(pred, self.weights0)
+		layer2 = self.think(layer1, self.weights1)
+		print(layer2)
+
 prepare_input_and_output_data()
 ann = NeuralNetwork()
 
 training_set_inputs = array(input_data)
-training_set_outputs = array(output_data).T
+training_set_outputs = array(output_data).T#TRANSPOSE MAKES IT A COLUMN VECTOR
+
 ann.train(training_set_inputs, training_set_outputs, 10000)
-#NEW PREDICTION
-op=ann.think(training_set_inputs[140])
-print(op)
+ann.prediction()
