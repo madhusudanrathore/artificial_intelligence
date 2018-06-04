611c222ee0855ec7985ee84b9d0c498c12a22f57
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-06-04 14:59:21 +0530

    Update and rename xml_to_list.py to retrieve_data.py

--- natural_language_processing/word_sense_disambiguation/retrieve_data.py
+++ natural_language_processing/word_sense_disambiguation/retrieve_data.py
@@ -1,21 +1,26 @@
 import xml.etree.ElementTree as ET
 
+'''THIS FILE DOES THE FOLLOWING'''
+'''1)	READ DATA FROM XML FILE'''
+'''2)	SAVE LIST OF TOKENIZED SENTENCES IN TXT FILE'''
+'''3)	SAVE WORDS WITH THEIR POS TAGS IN A CSV FILE'''
+
 '''37176 SENTENCES'''
 '''700201 WORDS AFTER REMOVING PUNCTUATIONS FROM EVERY SENTENCE'''
 '''700201 POS TAGS FOR EACH WORD'''
-'''802443 WORDS IF INCLUDING PUNCTUATION'''
 '''43982 VOCABULARY'''
+
 tree = ET.parse('../data/semcor.data.xml')
 root = tree.getroot()#GET ROOT OF XML DOCUMENT
 
 sentence_list=[]
-pos_tags=[]
 
+pos_word_csv_file_writer = open("stored_train_data/word_pos.csv", "w")
 '''READ SENTENCES AND POS TAGS FOR WORDS FROM XML FILE'''
 for sentence in root.iter('sentence'):#LOOP EACH SENTENCE TAG
 	temp_sentence=[]
 	for child in sentence.iter():#LOOP EACH WORD OF A SENTENCE
-		temp_word=child.text
+		temp_word=child.text#GET WORD
 		temp_pos=child.get('pos')#GET POS TAG FOR CORRESPONDING WORD
 		if temp_pos is not None and temp_pos != '.' and temp_word is not '\n':#PRE CONDITIONS, REMOVES PUNCTUATIONS
 		#if temp_pos is not None and temp_word is not '\n':#PRE CONDITIONS, DOESN'T REMOVES PUNCTUATIONS
@@ -23,15 +28,11 @@ for sentence in root.iter('sentence'):#LOOP EACH SENTENCE TAG
 			temp_word=temp_word.lower()#CONVERT TO LOWERCASE
 			temp_word=" ".join(temp_word.split())#REMOVE ALL TYPES OF WHITE SPACES
 			temp_sentence.append(temp_word)
-			pos_tags.append(temp_pos)
+			pos_word_csv_file_writer.write("{},{}\n".format(temp_word,temp_pos))#WRITE WORDS AND POS TAGS
 	sentence_list.append(temp_sentence)
+pos_word_csv_file_writer.close()
 
-'''WRITE WORDS TO EXTERNAL FILE'''
-xml_words = open("stored_data/sentences.txt", "w")
-xml_words.write("{}".format(sentence_list))
-xml_words.close()
-
-'''WRITE POS TAGS TO EXTERNAL FILE'''
-xml_pos = open("stored_data/pos.txt", "w")
-xml_pos.write("{}".format(pos_tags))
-xml_pos.close()
+'''WRITE TOKENIZED SENTENCES TO EXTERNAL FILE'''
+tokenized_sentences = open("stored_train_data/sentences.txt", "w")
+tokenized_sentences.write("{}".format(sentence_list))
+tokenized_sentences.close()
